30fd2cad67c7e181e89380369df3601d838cbdcf
azu <azusahmr@k-on.kr>
2025-06-16 01:27:51 +0900

    feat: vercel 분석기 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 import { AppSidebar } from "@/components/app-sidebar";
@@ -24,6 +26,8 @@ export default function RootLayout({
                     <AppSidebar />
                     <SidebarInset>{children}</SidebarInset>
                 </SidebarProvider>
+                <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
